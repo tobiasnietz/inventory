38d45a2e64785d109676a812f82342f07f1e2e22
SaphireDark <saphire-github@outlook.de>
2024-01-05 12:53:03 +0100

    Delete Version 2.0

--- src/main/java/com/thwildau/inventory/InventoryController.java
+++ src/main/java/com/thwildau/inventory/InventoryController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class InventoryController {
@@ -35,11 +37,11 @@ public class InventoryController {
 
     @GetMapping("/delete/{id}")
     public void delelem(@PathVariable Long id) {
-        try {
-            inventoryRepository.deleteById(id);
-        } catch (Exception e) {
-            throw new InventoryNotFoundException(id);
-        }
+
+        inventoryRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        inventoryRepository.deleteById(id);
     }
 
     @GetMapping("/inventory/location/{location}")
